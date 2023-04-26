e35adbcd0295b65cf953c07b0332a422d2859ffa
Vishnu Prabhakar <vishnuprabhakar@Vishnus-MacBook-Pro.local>
2023-04-26 03:18:08 -0400

    Updates for existing records

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 let myForm = document.getElementById("myForm");
 
 function addRow(tableID) {
-    var table = document.getElementById(tableID);
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
-    var colCount = table.rows[0].cells.length;
-    for(var i=0; i<colCount; i++) {
-        var newcell	= row.insertCell(i);
-        switch(i) {
-            case 0:
-                let firstName = document.getElementById("firstName").value;
-                newcell.innerHTML = firstName;
-                break;
-            case 1:
-                let lastName = document.getElementById("lastName").value;
-                newcell.innerHTML = lastName;
-                break;
-            case 2:
-                let age = document.getElementById("age").value;
-                newcell.innerHTML = age;
-                break;
-            default:
-                break;
+    let table = document.getElementById(tableID);
+    let rowCount = table.rows.length;
+    let colCount = table.rows[0].cells.length;
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let age = document.getElementById("age").value;
+
+    let recordAlreadyExists = false;
+    for(let loopVar = 0; loopVar < rowCount; loopVar++) {
+        let existingRow = table.rows[loopVar];
+
+        let firstNameMatched = false;
+        let lastNameMatched = false;
+        let ageMatched = false;
+
+        for(let colLoopVar = 0; colLoopVar < colCount; colLoopVar++) {
+            if(colLoopVar == 0 && firstName == existingRow.cells[colLoopVar].innerHTML) {
+                firstNameMatched = true;
+            }
+
+            if(colLoopVar == 1 && lastName == existingRow.cells[colLoopVar].innerHTML) {
+                lastNameMatched = true;
+            }
+
+            if(colLoopVar == 2 && age == parseInt(existingRow.cells[colLoopVar].innerHTML)) {
+                ageMatched = true;
+            }
         }
-        
+
+        if(firstNameMatched && lastNameMatched && ageMatched) {
+            recordAlreadyExists = true;
+            break;
+        }
     }
+
+    if(!recordAlreadyExists) {
+        let row = table.insertRow(rowCount);
+        for(let loopVar = 0; loopVar < colCount; loopVar++) {
+            var newcell	= row.insertCell(loopVar);
+            switch(loopVar) {
+                case 0:
+                    newcell.innerHTML = firstName;
+                    break;
+                case 1:
+                    newcell.innerHTML = lastName;
+                    break;
+                case 2:
+                    newcell.innerHTML = age;
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    } else {
+        alert("Record already exists!!!");
+    }
+    
 }
 
 if(myForm) {
